6ef5a77d958438b1c1a35546e8fd2e78601b3d3c
Andrew Nguyen <andrew@klarahealth.com>
2014-08-05 09:18:55 -0700

    Initial creation of new repo with lein new

---
+++ test/google_cloud_storage/core_test.clj
@@ -0,0 +1,7 @@
+(ns google-cloud-storage.core-test
+  (:require [clojure.test :refer :all]
+            [google-cloud-storage.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
